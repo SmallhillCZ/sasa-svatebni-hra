b9fe8a6d224e41bf8d609bf4ab0a51ef9cf69e06
Martin Kopeček <martin.kopecek@smallhill.cz>
2025-06-13 19:38:56 +0000

    chore: add debug log

--- backend/src/services/notifications.service.ts
+++ backend/src/services/notifications.service.ts
@@ -87,10 +87,11 @@ export class NotificationsService {
 				) {
 					this.logger.warn(`Subscription ${subscription.id} is no longer valid. Removing it.`);
 					await this.database.removeSubscription(subscription.id).catch(() => {});
-					console.error(error);
+
+					this.logger.error(JSON.stringify(error));
 				} else {
 					this.logger.error("Failed to send notification", error);
-					console.error(error);
+					this.logger.error(JSON.stringify(error));
 				}
 			}
 		}
